ca1cd9a82ac829353808c64fd4ad910008278ae2
Samantha <samanthagottlieb31@gmail.com>
2020-12-01 15:30:51 +0000

    Create Thermostat class with constructor, getCurrentTemperature and up methods

---
+++ src/thermostat.js
@@ -0,0 +1,13 @@
+'use strict';
+
+class Thermostat{
+  constructor() {
+    this.temperature = 20;
+  }
+  getCurrentTemperature() {
+    return this.temperature;
+  }
+  up() {
+    this.temperature += 1;
+  };
+};
