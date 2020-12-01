82c2a0107484ff898a8dc28c7b0d3e66cf493c07
Samantha <samanthagottlieb31@gmail.com>
2020-12-01 15:49:48 +0000

    Add minimum temperature

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -21,4 +21,11 @@ describe('Thermostat', function() {
     thermostat.down();
     expect(thermostat.getCurrentTemperature()).toEqual(19);
   })
+
+  it('has a minimum of 10 degrees', function() {
+    for (var i = 0; i < 11; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,6 +2,7 @@
 
 class Thermostat{
   constructor() {
+    this.MINIMUM_TEMPERATURE = 10;
     this.temperature = 20;
   }
   getCurrentTemperature() {
@@ -11,6 +12,12 @@ class Thermostat{
     this.temperature += 1;
   }
   down() {
+    if (this.isMinimumTemperature()) {
+      return;
+    }
     this.temperature -= 1;
+  }
+  isMinimumTemperature() {
+    return this.temperature === this.MINIMUM_TEMPERATURE;
   }
 };
