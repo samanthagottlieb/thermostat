7b670b3cd4c3b773e5d18a9a72345cba3ac2fc62
Samantha <samanthagottlieb31@gmail.com>
2020-12-01 16:01:16 +0000

    Add power saving mode
    Creating powerSavingMode property and getter method, and methods to switch powerSavingMode off and on

--- src/thermostat.js
+++ src/thermostat.js
@@ -4,6 +4,7 @@ class Thermostat{
   constructor() {
     this.MINIMUM_TEMPERATURE = 10;
     this.temperature = 20;
+    this.powerSavingMode = true;
   }
   getCurrentTemperature() {
     return this.temperature;
@@ -19,5 +20,14 @@ class Thermostat{
   }
   isMinimumTemperature() {
     return this.temperature === this.MINIMUM_TEMPERATURE;
+  }
+  isPowerSavingModeOn() {
+    return this.powerSavingMode === true;
+  }
+  switchPowerSavingModeOff() {
+    this.powerSavingMode = false;
+  }
+  switchPowerSavingModeOn() {
+    this.powerSavingMode = true;
   }
 };
