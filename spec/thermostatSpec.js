4bcd9eff45e42c2d260d62e0c2cc00838df9b7dd
Samantha <samanthagottlieb31@gmail.com>
2020-12-01 15:37:36 +0000

    Create down method
    To reduce temperature by 1

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -16,4 +16,9 @@ describe('Thermostat', function() {
     thermostat.up();
     expect(thermostat.getCurrentTemperature()).toEqual(21);
   });
+
+  it('decreases in temperature with down()', function() {
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  })
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -9,5 +9,8 @@ class Thermostat{
   }
   up() {
     this.temperature += 1;
-  };
+  }
+  down() {
+    this.temperature -= 1;
+  }
 };
