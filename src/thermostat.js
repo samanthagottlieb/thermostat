db088621234305c922f0f605671c51777c2b8eb7
Samantha <samanthagottlieb31@gmail.com>
2020-12-03 13:03:32 +0000

    Implement upper temperature limits
    Adding max limit properties for power saving mode being on or off, and adjusting up and isMaximumTemperature methods

--- src/thermostat.js
+++ src/thermostat.js
@@ -5,11 +5,16 @@ class Thermostat{
     this.MINIMUM_TEMPERATURE = 10;
     this.temperature = 20;
     this.powerSavingMode = true;
+    this.MAX_LIMIT_PSM_ON = 25;
+    this.MAX_LIMIT_PSM_OFF = 32;
   }
   getCurrentTemperature() {
     return this.temperature;
   }
   up() {
+    if (this.isMaximumTemperature()) {
+      return;
+    }
     this.temperature += 1;
   }
   down() {
@@ -20,6 +25,12 @@ class Thermostat{
   }
   isMinimumTemperature() {
     return this.temperature === this.MINIMUM_TEMPERATURE;
+  }
+  isMaximumTemperature() {
+    if (this.isPowerSavingModeOn() === false) {
+      return this.temperature === this.MAX_LIMIT_PSM_OFF;
+    }
+    return this.temperature === this.MAX_LIMIT_PSM_ON;
   }
   isPowerSavingModeOn() {
     return this.powerSavingMode === true;
